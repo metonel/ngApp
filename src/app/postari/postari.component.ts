03b4a3d28130049b14858bc85751c6c21f3bf441
Toni Nemtisor <nemtisor.antonio@gmail.com>
2018-06-20 15:37:37 +0300

    animations pt postari

--- src/app/postari/postari.component.ts
+++ src/app/postari/postari.component.ts
@@ -1,12 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { Observable } from 'rxjs';
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
 
 @Component({
   providers: [DataService],
   selector: 'app-postari',
   templateUrl: './postari.component.html',
-  styleUrls: ['./postari.component.scss']
+  styleUrls: ['./postari.component.scss'],
+  animations :[
+    trigger('listStagger', [
+      transition('* <=> *', [
+        query(':enter', [
+          style({ opacity: 0, transform: 'translateY(-15px)'}),
+          stagger('50ms', 
+          animate('550ms ease-out',
+          style({ opacity: 1, transform: 'translateY(0px)'})))
+        ], { optional: true }),
+        query(':leave', animate('50ms', style({ opacity: 0 })), 
+        { optional: true })
+      ])
+    ])
+  ]
 })
 export class PostariComponent implements OnInit {
 
